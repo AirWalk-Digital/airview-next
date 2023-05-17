09493d31ba76a3f0d760ff9310c2dc99c6d211f7
Rob Ellison <rob.ellison@r&r.org.uk>
2023-05-17 12:48:30 +0000

    fix: tidy index.js

--- pages/index.js
+++ pages/index.js
@@ -74,12 +74,8 @@ return (
 
 
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// const theme = createTheme();
-
 export default function Home() {
-  // const theme = useTheme();
+
   const [padList, setPadList] = useState(0);
   const [refreshToken, setRefreshToken] = useState(Math.random());
 
@@ -88,7 +84,6 @@ export default function Home() {
     .then((res) => res.json())
     .then(data => {
       setPadList(data.pads)
-      // console.log('data : ', data )
       })
       .catch(error => {
         console.log(error)
@@ -156,109 +151,13 @@ export default function Home() {
           </Grid>
 
 
-          {/* <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid> */}
         </Container>
       </main>
       {/* Footer */}
       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        {/* <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography> */}
         <Copyright />
       </Box>
       {/* End footer */}
     </ThemeProvider>
   );
 }
-
-
-
-
-
-
-
-
-// export default function () {
-//   const [padList, setPadList] = useState(0);
-//   const [refreshToken, setRefreshToken] = useState(Math.random());
-
-//   useEffect(() => {
-//     fetch(`/api/etherpad/listAllPads`)
-//     .then((res) => res.json())
-//     .then(data => {
-//       setPadList(data.pads)
-//       // console.log('data : ', data )
-//       })
-//       .catch(error => {
-//         console.log(error)
-//       })
-//       .finally(() => {
-//         setTimeout(() => setRefreshToken(Math.random()), 5000);
-//       });
-  
-//   }, [refreshToken]);
-
-
-//   return (
-//     <>
-      
-//       <br />
-//       {padList ? (
-//         <>
-
-
-//           Available pads from Etherpad:
-//           <br />
-//           <h2>Presentations</h2>
-//           {
-//             padList.map((pad, i) => (
-//               <Box key={i}>
-//                 <Link  href={`/pads/ppt/${pad}`}>{pad}</Link>
-//               </Box>
-//             ))
-//           }
-//         </>
-//       ) : 'Etherpad is not running'
-//       }
-//     </>
-//   )
-// }
