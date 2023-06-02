ab9ed6f1b94cf3aecf41bf27b10612e3114ca388
Rob Ellison <rob.ellison@r&r.org.uk>
2023-06-02 13:03:56 +0000

    fix: styles on control table

--- components/ControlFrameworkTables.jsx
+++ components/ControlFrameworkTables.jsx
@@ -17,10 +17,28 @@ import * as React from 'react';
 
 
 import { useState } from 'react';
-import { DataGrid, GridCellExpand } from '@mui/x-data-grid';
-import { IconButton, Dialog, DialogTitle, DialogContent } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { IconButton, Button, Dialog, DialogTitle, DialogContent, styled } from '@mui/material';
 import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
-import { Info } from '@mui/icons-material';
+import { Info, Close } from '@mui/icons-material';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { baseTheme } from '../constants/baseTheme';
+
+const theme = createTheme({
+  components: {
+    MuiDataGrid: {
+      styleOverrides: {
+        root: {
+          '& .MuiDataGrid-columnHeaders': {
+            backgroundColor: baseTheme.palette.background.primary,
+            color: 'white',
+          },
+        },
+      },
+    },
+  },
+});
+
 
 export const FrameworkCoverageTable = ({ rows, sx }) => {
   const [selectedRow, setSelectedRow] = useState(null);
@@ -50,10 +68,12 @@ export const FrameworkCoverageTable = ({ rows, sx }) => {
   const columns = [
     { field: 'appName', headerName: 'Application Name', flex: 1 },
     { field: 'businessUnit', headerName: 'Business Unit', flex: 1 },
+    { field: 'exemptions', headerName: 'Exemptions', flex: 1 },
     {
-      field: 'exemptions',
-      headerName: 'Exemptions',
-      flex: 1,
+      field: '',
+      headerName: ' ',
+      width: 50,
+      align: 'right',
       renderCell: (params) => (
         <>
           {params.value}
@@ -66,40 +86,51 @@ export const FrameworkCoverageTable = ({ rows, sx }) => {
   const rowsWithId = rows.map((row) => ({ ...row, id: row.id.toString() }));
 
   return (
-    <>
-        <DataGrid
-          rows={rowsWithId}
-          columns={columns}
-          sx={{...sx}}
-        />
-      <Dialog open={open} onClose={handleDialogClose}>
+    <ThemeProvider theme={theme}>
+      <DataGrid
+        rows={rowsWithId}
+        columns={columns}
+        sx={{ ...sx }}
+      />
+      <Dialog open={open} onClose={handleDialogClose} maxWidth="md" fullWidth>
         {selectedRow && (
           <>
-            <DialogTitle>{selectedRow.appName} Controls</DialogTitle>
+            <DialogTitle>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <span style={{ flex: 1 }}>{selectedRow.appName} Controls</span>
+                <Button
+                  onClick={handleDialogClose}
+                  startIcon={<Close />}
+                  color="primary"
+                >
+                  Close
+                </Button>
+              </div>
+            </DialogTitle>
             <DialogContent>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Control Name</th>
-                    <th>Issues</th>
-                    <th>Criticality</th>
-                  </tr>
-                </thead>
-                <tbody>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Control Name</TableCell>
+                    <TableCell>Issues</TableCell>
+                    <TableCell>Criticality</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
                   {selectedRow.controls.map((control) => (
-                    <tr key={control.name}>
-                      <td>{control.name}</td>
-                      <td>{control.issues}</td>
-                      <td>{control.criticality}</td>
-                    </tr>
+                    <TableRow key={control.name}>
+                      <TableCell>{control.name}</TableCell>
+                      <TableCell>{control.issues}</TableCell>
+                      <TableCell>{control.criticality}</TableCell>
+                    </TableRow>
                   ))}
-                </tbody>
-              </table>
+                </TableBody>
+              </Table>
             </DialogContent>
           </>
         )}
       </Dialog>
-    </>
+    </ThemeProvider>
   );
 };
 
@@ -205,81 +236,81 @@ const columns = [
 
 
 const rows = [
-  { id: 1, appName: 'Airview', businessUnit: 'Cloud CoE', exemptions: 35, controls: [{name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  }]},
-  { id: 2, appName: 'Microsoft Teams', businessUnit: 'Central IT', exemptions: 42,controls: [{name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  }]},
-  { id: 3, appName: 'Public Website', businessUnit: 'Marketing', exemptions: 45, controls: [{name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  }]},
+  { id: 1, appName: 'Airview', businessUnit: 'Cloud CoE', exemptions: 35, controls: [{ name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }] },
+  { id: 2, appName: 'Microsoft Teams', businessUnit: 'Central IT', exemptions: 42, controls: [{ name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }] },
+  { id: 3, appName: 'Public Website', businessUnit: 'Marketing', exemptions: 45, controls: [{ name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }] },
 ];
 
 
-controls: [{name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  },{name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high'  }]
+controls: [{ name: 'AC1', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC2', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }, { name: 'AC3', name: 'Control Storage Accounts', issues: 5, criticality: 'high' }]
 
-export function FrameworkCoverageTableOld({sx}) {
+export function FrameworkCoverageTableOld({ sx }) {
   return (
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-        sx={{...sx}}
-      />
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      initialState={{
+        pagination: {
+          paginationModel: { page: 0, pageSize: 5 },
+        },
+      }}
+      pageSizeOptions={[5, 10]}
+      checkboxSelection
+      sx={{ ...sx }}
+    />
   );
 }
 
 function Implementation({ methods }) {
-    if (methods) {
-        return (
-            <TableCell align="right">
-                <Stack direction="column" spacing={0.2} >
-                    {methods.map((row, i) => (
-                        row.system && <Chip key={i} label={row?.system ?? 'n/a'} variant='outlined' size='small' color="success" />
-                    ))}
-                </Stack>
-            </TableCell>
-        )
+  if (methods) {
+    return (
+      <TableCell align="right">
+        <Stack direction="column" spacing={0.2} >
+          {methods.map((row, i) => (
+            row.system && <Chip key={i} label={row?.system ?? 'n/a'} variant='outlined' size='small' color="success" />
+          ))}
+        </Stack>
+      </TableCell>
+    )
 
-    } else {
-        return (
-            <TableCell align="right"></TableCell>
-        )
-    }
+  } else {
+    return (
+      <TableCell align="right"></TableCell>
+    )
+  }
 }
 export function ControlTable({ controls, framework = 'NIST80053' }) {
 
-    console.log('ControlTable:controls: ', controls)
+  console.log('ControlTable:controls: ', controls)
 
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead sx={{ color: 'background.primary' }} >
-                    <TableRow>
-                        <TableCell>Framework Domain</TableCell>
-                        <TableCell align="right">Control</TableCell>
-                        <TableCell align="left">Description</TableCell>
-                        <TableCell align="right">Implementation(s)</TableCell>
-                        <TableCell align="right">Owner</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {controls.map((row, i) => (
-                        <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell component="th" scope="row">
-                                {/* {row.data?.frameworks?.find(f => f.name === framework)?.mapping?.section ?? null} */}
-                                {row.data?.frameworks?.filter(f => f.name === framework).flatMap(f => f.mapping?.map(m => m.domain)).join(", ") || null}
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead sx={{ color: 'background.primary' }} >
+          <TableRow>
+            <TableCell>Framework Domain</TableCell>
+            <TableCell align="right">Control</TableCell>
+            <TableCell align="left">Description</TableCell>
+            <TableCell align="right">Implementation(s)</TableCell>
+            <TableCell align="right">Owner</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {controls.map((row, i) => (
+            <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row">
+                {/* {row.data?.frameworks?.find(f => f.name === framework)?.mapping?.section ?? null} */}
+                {row.data?.frameworks?.filter(f => f.name === framework).flatMap(f => f.mapping?.map(m => m.domain)).join(", ") || null}
 
-                            </TableCell>
-                            <TableCell align="right">{row?.data?.id ?? null}</TableCell>
-                            <TableCell align="left">{row?.data?.description ?? null}</TableCell>
-                            <TableCell component={Implementation} align="right" methods={row?.data?.methods ?? null} />
-                            <TableCell align="right">{row?.data?.control_owner ?? null}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+              </TableCell>
+              <TableCell align="right">{row?.data?.id ?? null}</TableCell>
+              <TableCell align="left">{row?.data?.description ?? null}</TableCell>
+              <TableCell component={Implementation} align="right" methods={row?.data?.methods ?? null} />
+              <TableCell align="right">{row?.data?.control_owner ?? null}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
