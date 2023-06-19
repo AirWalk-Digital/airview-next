9d4a60c543d54eb4d0aaccb258db9b17dd701d93
Rob Ellison <rob.ellison@r&r.org.uk>
2023-06-19 18:23:03 +0000

    feat: add knowledge

--- components/solutions/SolutionView.jsx
+++ components/solutions/SolutionView.jsx
@@ -1,66 +1,38 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 
-import { ErrorBoundary } from 'react-error-boundary'
 import { baseTheme } from '../../constants/baseTheme';
 
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { mdComponents } from "../../constants/mdxProvider";
 import Link from '@mui/material/Link';
-import { Stack } from '@mui/material'
-import { Chip } from '@mui/material'
 import { TopBar } from '@/components/dashboard';
-import { MiniStatisticsCard } from "@/components/dashboard";
-import { MDXProvider } from '@mdx-js/react';
-import { Menu, ButtonMenu, NavigationDrawer } from '@/components/airview-ui';
-import { ControlTable } from '@/components/compliance/ControlTable';
-import { ControlDataDisplay } from '@/components/compliance/ControlData';
-import { useMdx } from '@/lib/content/mdx'
-import Container from '@mui/material/Container';
-import CloseIcon from '@mui/icons-material/Close';
+import { Menu, NavigationDrawer } from '@/components/airview-ui';
 import { PagedOutput } from '@/components/display/PagedOutput';
 import { PresentationOutput } from '@/components/display/PresentationOutput';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, IconButton, Typography, MenuItem, Box, Alert, Grid } from '@mui/material';
-import { Container as MuiContainer } from "@mui/material";
 import SlideshowIcon from '@mui/icons-material/Slideshow';
+import { IconButton, Typography, MenuItem, Box, Alert, Grid } from '@mui/material';
 
-import {
-
-  AsideAndMainContainer,
-  Aside,
-  Main,
-
-} from '@/components/airview-ui';
-
-import { Previewer } from 'pagedjs'
-
-import { Tile } from '@/components/dashboard/Tiles'
-import path from 'path';
-
-import { siteConfig } from "../../site.config.js";
+import { AsideAndMainContainer, Aside, Main } from '@/components/airview-ui';
 
 export function SolutionView({
-  knowledge,
-  solutions,
-  designs,
   children,
   frontmatter,
   file,
-  content
+  content,
+  menuStructure
 }) {
 
 
+  console.log('SolutionView:menuStructure: ', menuStructure)
   const navDrawerWidth = 300;
   const topBarHeight = 64;
   const [menuOpen, setMenuOpen] = useState(true);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [controlUrl, setControlUrl] = useState('');
   const [print, setPrint] = useState(false);
   const [presentation, setPresentation] = useState(false);
 
   const handleOnNavButtonClick = () => setMenuOpen((prevState) => !prevState);
 
-  const { solutionMenu, chapterFiles, knowledgeFiles, designFiles } = fileStructure(solutions)
+  const { solutionMenu, chapterFiles, knowledgeFiles, designFiles } = menuStructure;
 
   function handlePrint() {
     setPrint(!print);
@@ -120,12 +92,10 @@ export function SolutionView({
               {children && children}
             </Main>
             <Aside sx={{ displayPrint: 'none', display: print ? 'none' : '' }}>
-              <ChaptersMenu
+              <ContentMenu
                 chapters={chapterFiles}
+                knowledge={knowledgeFiles}
                 file={file}
-                open={menuOpen}
-                top={topBarHeight}
-                drawerWidth={navDrawerWidth}
               />
               {/* <ButtonMenu
                 menuTitle="Controls"
@@ -153,34 +123,49 @@ export function SolutionView({
   } else if (presentation) {
     return (
       <PresentationOutput handlePresentation={handlePresentation} refresh={false} content={content}>
-          {children && children}
+        {children && children}
       </PresentationOutput>
     )
   }
 }
 
 
-function ChaptersMenu({ chapters, file, open, top, drawerWidth }) {
+function ContentMenu({ chapters, file, knowledge }) {
   // console.log('ChaptersMenu: ', chapters)
   // console.log('ChaptersMenu:File ', file)
   let directory = file.split("/")[1]; // Extract directory name
-  const chaptersMenu = [
-    {
-      groupTitle: "",
-      links: chapters[directory]
-    }
-  ]
+  let chaptersMenu = []
+  if (chapters[directory]) {
+    chaptersMenu.push(
+      {
+        groupTitle: "Chapters",
+        links: chapters[directory]
+      }
+    )
+  }
+  if (knowledge[directory]) {
+    chaptersMenu.push(
+      {
+        groupTitle: "Knowledge",
+        links: knowledge[directory]
+      }
+    )
+  }
 
-  return (
-    <Menu
-      menuTitle="Chapters"
-      menuItems={chaptersMenu}
-      initialCollapsed={false}
-      loading={false}
-      fetching={false}
-    />
-  );
+  if (chaptersMenu) {
+    return (
+      <Menu
+        menuTitle="Content"
+        menuItems={chaptersMenu}
+        initialCollapsed={false}
+        loading={false}
+        fetching={false}
+      />
+    );
+  }
 }
+
+
 
 
 function SolutionsMenu({ solutions, open, top, drawerWidth }) {
@@ -196,59 +181,4 @@ function SolutionsMenu({ solutions, open, top, drawerWidth }) {
         {c.label}</MenuItem></Link>)}
     </NavigationDrawer>
   );
-}
-
-
-function fileStructure(solutions, knowledge) {
-  const solutionMenu = [];
-  const chapterFiles = {};
-  const indexFiles = new Set();
-
-  // First pass: Find index.md files
-  for (let x of solutions) {
-    if (
-      x.file &&
-      x.file.split('/').length === 3 &&
-      x.file.match(/(_index\.md*|index\.md*)$/) &&
-      x.frontmatter &&
-      x.frontmatter.title
-    ) {
-      solutionMenu.push({
-        label: x.frontmatter.title,
-        url: x.file,
-      });
-      indexFiles.add(x.file.split("/")[1]); // Add directory name to the Set
-    }
-  }
-
-  // Second pass: Process non-index.md files
-  for (let x of solutions) {
-    if (
-      x.file &&
-      x.file.split('/').length > 2 && // skip any files in the root of the directory
-      !x.file.match(/(_index\.md*|index\.md*)$/) &&
-      x.frontmatter &&
-      x.frontmatter.title
-    ) {
-      let directory = x.file.split("/")[1]; // Extract directory name
-
-      // Only add file to solutionMenu if there is no corresponding index.md
-      if (!indexFiles.has(directory)) {
-        solutionMenu.push({
-          label: x.frontmatter.title,
-          url: x.file,
-        });
-      }
-
-      // Check if the key exists in the chapterFiles object
-      if (!chapterFiles[directory]) {
-        chapterFiles[directory] = [];
-      }
-      chapterFiles[directory].push({
-        label: x.frontmatter.title,
-        url: '/' + x.file,
-      });
-    }
-  }
-  return { solutionMenu, chapterFiles, knowledgeFiles: null, designFiles: null };
 }
--- pages/solutions/[...solution].jsx
+++ pages/solutions/[...solution].jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react'
 import { siteConfig } from "../../site.config.js";
 import { mdComponents } from "../../constants/mdxProvider";
 import * as matter from 'gray-matter';
-import { parse } from 'toml';
 import { MDXProvider } from '@mdx-js/react';
 import { getAllFiles, getFileContent } from '@/lib/github'
 import { useMDX } from '@/lib/content/mdx'
@@ -12,13 +11,12 @@ import { SolutionView } from '@/components/solutions'
 
 import { FullScreenSpinner } from '@/components/dashboard/index.js';
 import { dirname, basename } from 'path';
+import { getMenuStructureSolutions } from '@/lib/content/menus';
 
 export default function Page({
-  knowledge,
-  solutions,
-  designs,
   content,
-  file }) {
+  file,
+  menuStructure }) {
 
   const [pageContent, setContent] = useState({ content: undefined, frontmatter: undefined });
 
@@ -40,13 +38,13 @@ export default function Page({
 
   if (pageContent.content && pageContent.frontmatter) {
     const Content = pageContent.content;
-    return <SolutionView solutions={solutions} designs={designs} knowledge={knowledge} frontmatter={pageContent.frontmatter} file={file} content={content} >
+    return <SolutionView frontmatter={pageContent.frontmatter} file={file} content={content} menuStructure={menuStructure} >
       <MDXProvider components={mdComponents(context)}>
         <Content />
       </MDXProvider>
     </SolutionView>
   } else {
-    return <SolutionView solutions={solutions} designs={designs} knowledge={knowledge} frontmatter={pageContent.frontmatter} file={file} >
+    return <SolutionView frontmatter={pageContent.frontmatter} file={file} menuStructure={menuStructure}>
       <MDXProvider components={mdComponents(context)}>
         <FullScreenSpinner />
       </MDXProvider>
@@ -61,9 +59,6 @@ export async function getStaticPaths() {
   let pages = [];
   try {
     const solutions = await getAllFiles(siteConfig.content.solutions.owner, siteConfig.content.solutions.repo, siteConfig.content.solutions.branch, siteConfig.content.solutions.path, true, '.md*');
-    // const pages = services.map((file) => {
-    //   return file
-    // })
     pages = solutions
       .filter((file) => basename(file) !== 'README.md')
       .map((file) => { return '/' + file });
@@ -87,65 +82,66 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
 
-  const solutions = await getAllFiles(siteConfig.content.solutions.owner, siteConfig.content.solutions.repo, siteConfig.content.solutions.branch, siteConfig.content.solutions.path, true, '.md*');
-  const knowledge = await getAllFiles(siteConfig.content.knowledge.owner, siteConfig.content.knowledge.repo, siteConfig.content.knowledge.branch, siteConfig.content.knowledge.path, true, '.md*');
 
-  const solutionsContentPromises = solutions.map((file) => {
-    return getFileContent(
-      siteConfig.content.solutions.owner,
-      siteConfig.content.solutions.repo,
-      siteConfig.content.solutions.branch,
-      file
-    )
-      .then(content => {
-        const matterData = matter(content, { excerpt: false }).data || null;
-        if (matterData) {
-          for (let key in matterData) {
-            if (matterData[key] instanceof Date) {
-              matterData[key] = matterData[key].toISOString();
-            }
-          }
-        }
-        return { file: file, frontmatter: matterData };
-      })
-      .catch(error => {
-        // console.error(`Error processing file ${file}: ${error}`);
-        return { file: null, frontmatter: null };
-      });
-  });
 
-  const solutionsContent = await Promise.all(solutionsContentPromises);
+  // const solutions = await getAllFiles(siteConfig.content.solutions.owner, siteConfig.content.solutions.repo, siteConfig.content.solutions.branch, siteConfig.content.solutions.path, true, '.md*');
+  // const knowledge = await getAllFiles(siteConfig.content.knowledge.owner, siteConfig.content.knowledge.repo, siteConfig.content.knowledge.branch, siteConfig.content.knowledge.path, true, '.md*');
 
-  const knowledgeContentPromises = knowledge.map((file) => {
-    return getFileContent(
-      siteConfig.content.knowledge.owner,
-      siteConfig.content.knowledge.repo,
-      siteConfig.content.knowledge.branch,
-      file
-    )
-      .then(content => {
-        const matterData = matter(content, { excerpt: false }).data || null;
-        return { file: file, frontmatter: matterData };
-      })
-      .catch(error => {
-        console.error(`Error processing file ${file}: ${error}`);
-        return null; // or however you want to handle errors for each file
-      });
-  });
+  // const solutionsContentPromises = solutions.map((file) => {
+  //   return getFileContent(
+  //     siteConfig.content.solutions.owner,
+  //     siteConfig.content.solutions.repo,
+  //     siteConfig.content.solutions.branch,
+  //     file
+  //   )
+  //     .then(content => {
+  //       const matterData = matter(content, { excerpt: false }).data || null;
+  //       if (matterData) {
+  //         for (let key in matterData) {
+  //           if (matterData[key] instanceof Date) {
+  //             matterData[key] = matterData[key].toISOString();
+  //           }
+  //         }
+  //       }
+  //       return { file: file, frontmatter: matterData };
+  //     })
+  //     .catch(error => {
+  //       // console.error(`Error processing file ${file}: ${error}`);
+  //       return { file: null, frontmatter: null };
+  //     });
+  // });
 
-  const knowledgeContent = await Promise.all(knowledgeContentPromises);
+  // const solutionsContent = await Promise.all(solutionsContentPromises);
+
+  // const knowledgeContentPromises = knowledge.map((file) => {
+  //   return getFileContent(
+  //     siteConfig.content.knowledge.owner,
+  //     siteConfig.content.knowledge.repo,
+  //     siteConfig.content.knowledge.branch,
+  //     file
+  //   )
+  //     .then(content => {
+  //       const matterData = matter(content, { excerpt: false }).data || null;
+  //       return { file: file, frontmatter: matterData };
+  //     })
+  //     .catch(error => {
+  //       console.error(`Error processing file ${file}: ${error}`);
+  //       return null; // or however you want to handle errors for each file
+  //     });
+  // });
+
+  // const knowledgeContent = await Promise.all(knowledgeContentPromises);
 
   const file = 'solutions/' + context.params.solution.join('/')
   const pageContent = await getFileContent(siteConfig.content.solutions.owner, siteConfig.content.solutions.repo, siteConfig.content.solutions.branch, file);
   const pageContentText = pageContent ? Buffer.from(pageContent).toString("utf-8") : '';
+  const menuStructure = await getMenuStructureSolutions(siteConfig);
 
   return {
     props: {
-      solutions: solutionsContent,
-      knowledge: knowledgeContent,
-      designs: null,
       content: pageContentText || null,
       file: file,
+      menuStructure: menuStructure || null
     },
   };
 }
