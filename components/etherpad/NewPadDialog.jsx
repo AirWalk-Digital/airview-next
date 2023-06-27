1534593c66b38f82ce1de675caea5e2d635bd86b
Rob Ellison <rob.ellison@r&r.org.uk>
2023-06-27 19:17:40 +0000

    remove unused packages

--- components/etherpad/NewPadDialog.jsx
+++ components/etherpad/NewPadDialog.jsx
@@ -1,25 +1,12 @@
 
 import * as React from 'react';
-import { useState, useEffect, } from 'react';
-import { TopBar } from '@/components/dashboard';
-import Box from '@mui/material/Box';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
-import SlideshowIcon from '@mui/icons-material/Slideshow';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { FormControl, Link, Dialog, DialogTitle, DialogContent, ButtonGroup, DialogContentText, Select, MenuItem, DialogActions, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
-import { baseTheme } from '../../constants/baseTheme';
 import * as matter from 'gray-matter';
 import { v4 as uuidv4 } from 'uuid';
-import { dirname, basename } from 'path';
+import { dirname } from 'path';
 
 import { siteConfig } from "../../site.config.js";
 
