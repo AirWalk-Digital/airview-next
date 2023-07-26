a042a84ec44c25fa6c4d01798ef9455d68a2d908
Andy Payne <andy.payne@airwalkconsulting.com>
2023-07-26 15:07:23 +0100

    feat: update redis cluster constructor
    add required options to connect to redis cluster

--- site.config.js
+++ site.config.js
@@ -3,77 +3,83 @@ export const siteConfig = {
   company: `airwalkreply.com`,
   tagline: `technology, done right`,
   etherpad: {
-    url: 'https://pad.airview.airwalkconsulting.io'
+    url: "https://pad.airview.airwalkconsulting.io",
   },
   content: {
     services: {
-      source: 'github', // The source of the content
-      repo: 'airwalk_patterns', // The name of the repo
-      owner: 'airwalk-digital', // The owner of the repo
-      branch: '1-rob-ellison',
-      path: 'services',  // the base path for the content
-      reference: 'service', // how the collection is referred to in frontmatter links
-      collections: ['knowledge', 'designs']
-
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "services", // the base path for the content
+      reference: "service", // how the collection is referred to in frontmatter links
+      collections: ["knowledge", "designs"],
     },
     providers: {
-      source: 'github', // The source of the content
-      repo: 'airwalk_patterns', // The name of the repo
-      owner: 'airwalk-digital', // The owner of the repo
-      branch: '1-rob-ellison',
-      path: 'providers',  // the base path for the content
-      reference: 'provider', // how the collection is referred to in frontmatter links
-      collections: ['services']
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "providers", // the base path for the content
+      reference: "provider", // how the collection is referred to in frontmatter links
+      collections: ["services"],
     },
     solutions: {
-      source: 'github', // The source of the content
-      repo: 'airwalk_patterns', // The name of the repo
-      owner: 'airwalk-digital', // The owner of the repo
-      branch: '1-rob-ellison',
-      path: 'solutions',  // the base path for the content
-      reference: 'solution', // how the collection is referred to in frontmatter links
-      collections: ['knowledge', 'designs']
-
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "solutions", // the base path for the content
+      reference: "solution", // how the collection is referred to in frontmatter links
+      collections: ["knowledge", "designs"],
     },
     knowledge: {
-      source: 'github', // The source of the content
-      repo: 'airwalk_patterns', // The name of the repo
-      owner: 'airwalk-digital', // The owner of the repo
-      branch: 'main',
-      path: 'knowledge',  // the base path for the content
-      reference: 'knowledge', // how the collection is referred to in frontmatter links
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "knowledge", // the base path for the content
+      reference: "knowledge", // how the collection is referred to in frontmatter links
     },
     designs: {
-      source: 'github', // The source of the content
-      repo: 'airwalk_patterns', // The name of the repo
-      owner: 'airwalk-digital', // The owner of the repo
-      branch: 'main',
-      path: 'designs',  // the base path for the content
-      reference: 'design', // how the collection is referred to in frontmatter links
-      collections: ['knowledge']
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "designs", // the base path for the content
+      reference: "design", // how the collection is referred to in frontmatter links
+      collections: ["knowledge"],
     },
     customers: {
-      source: 'github', // The source of the content
-      repo: 'airwalk_patterns', // The name of the repo
-      owner: 'airwalk-digital', // The owner of the repo
-      branch: 'main',
-      path: 'customers',  // the base path for the content
-      reference: 'customer', // how the collection is referred to in frontmatter links
-      collections: ['knowledge', 'projects']
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "customers", // the base path for the content
+      reference: "customer", // how the collection is referred to in frontmatter links
+      collections: ["knowledge", "projects"],
     },
     projects: {
-      source: 'github', // The source of the content
-      repo: 'airwalk_patterns', // The name of the repo
-      owner: 'airwalk-digital', // The owner of the repo
-      branch: 'main',
-      path: 'projects',  // the base path for the content
-      reference: 'project', // how the collection is referred to in frontmatter links
-      collections: ['knowledge']
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "projects", // the base path for the content
+      reference: "project", // how the collection is referred to in frontmatter links
+      collections: ["knowledge"],
     },
-    applications: { source: 'static'},
-    frameworks: { source: 'static'}
-  }
+    products: {
+      source: "github", // The source of the content
+      repo: "airview-demo-content", // The name of the repo
+      owner: "airwalk-digital", // The owner of the repo
+      branch: "main",
+      path: "products", // the base path for the content
+      reference: "product", // how the collection is referred to in frontmatter links
+      collections: ["knowledge", "designs"],
+    },
+    applications: { source: "static" },
+    frameworks: { source: "static" },
+  },
+};
 
-}
-
-export default siteConfig
+export default siteConfig;
