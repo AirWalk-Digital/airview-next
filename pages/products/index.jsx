6976b969acc8b9da66212b675241106e069997ea
Rob Ellison <rob.ellison@r&r.org.uk>
2023-08-03 15:47:33 +0000

    feat: cache products

--- pages/products/index.jsx
+++ pages/products/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { siteConfig } from "../../site.config.js";
-import * as matter from "gray-matter";
-import { getAllFiles, getFileContent } from "@/lib/github";
 import { IndexView } from "@/components/content";
-import { getMenuStructure, groupMenu } from "@/lib/content";
+import { getMenuStructure, groupMenu, getFrontMatter } from "@/lib/content";
 
 export default function Page({ tiles, menuStructure }) {
   return (
@@ -11,35 +9,6 @@ export default function Page({ tiles, menuStructure }) {
   );
 }
 
-async function getFrontMatter(config) {
-  const files = await getAllFiles(
-    config.owner,
-    config.repo,
-    config.branch,
-    config.path,
-    true,
-    ".md*"
-  );
-  const filesPromises = files.map((file) => {
-    return getFileContent(config.owner, config.repo, config.branch, file)
-      .then((content) => {
-        const matterData = matter(content, { excerpt: false }).data || null;
-        if (matterData) {
-          for (let key in matterData) {
-            if (matterData[key] instanceof Date) {
-              matterData[key] = matterData[key].toISOString();
-            }
-          }
-        }
-        return { file: file, frontmatter: matterData };
-      })
-      .catch((error) => {
-        // console.error(`Error processing file ${file}: ${error}`);
-        return { file: null, frontmatter: null };
-      });
-  });
-  return await Promise.all(filesPromises);
-}
 
 export async function getServerSideProps(context) {
   // export async function getServerSideProps(context) {
