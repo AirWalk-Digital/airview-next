08225a9092ed9fb28f8c2d69a16f365379d39f56
Rob Ellison <rob.ellison@r&r.org.uk>
2023-12-27 23:07:12 +0000

    feat: add /api/update path to pre-cache menus

--- pages/api/update/index.js
+++ pages/api/update/index.js
@@ -1,7 +1,27 @@
 // update Cache
+import { siteConfig } from "../../../site.config.js";
+import { getMenuStructure } from "@/lib/content";
+
 
 export default async function handler(req, res) {
   try {
+    for (let key in siteConfig.content) {
+      let initialContext = siteConfig.content[key];
+      console.log("API:update:collection: ", key);
+      const collection = () => {
+        if (initialContext.menu && initialContext.menu.collection) {
+          console.debug('usePageContent:collection: ', siteConfig.content[siteConfig.content[initialContext.menu.collection].path])
+          return siteConfig.content[siteConfig.content[initialContext.menu.collection].path]
+        } else {
+          console.debug('usePageContent:collection: ', initialContext)
+          return initialContext
+        }
+      }
+      const initialMenuStructure = await getMenuStructure(siteConfig, collection());
+      
+
+    }
+
     res.status(200).json({ status: "success" });
   } catch (error) {
     res
