91786c44faa6028e90eada5ac4a80f89dca9fbde
Rob Ellison <rob.ellison@r&r.org.uk>
2023-12-29 23:48:42 +0000

    fix: cache errors

--- pages/[[...path]]/index.jsx
+++ pages/[[...path]]/index.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { siteConfig } from "../../site.config.js";
 import { parse } from "toml";
 import { getAllFiles, getFileContent } from "@/lib/github";
-import { usePageContent, collectionName, usePageMenu, LeftMenuFunction, LeftMenu, LeftMenuOpen } from "@/lib/hooks";
+import {
+  usePageContent,
+  collectionName,
+  usePageMenu,
+  LeftMenuFunction,
+  LeftMenu,
+  LeftMenuOpen,
+} from "@/lib/hooks";
 import { getMenuStructure, groupMenu, getFrontMatter } from "@/lib/content";
 
 import { ContentPage, IndexView } from "@/components/layouts";
@@ -18,10 +25,9 @@ export default function Page({
   tiles,
   loading,
 }) {
-
   if (type === "home") {
     return <LandingPage />;
-  } else if (type ==='404') {
+  } else if (type === "404") {
     return <LandingPage />;
   } else if (type === "index") {
     if (loading) {
@@ -55,13 +61,9 @@ export default function Page({
       context,
       content,
       editMode,
-    } = usePageContent(
-      initialContent,
-      initialContext
-    );
+    } = usePageContent(initialContent, initialContext);
 
     console.debug("[[...path]]/index:editMode: ", editMode);
-
 
     return (
       <ContentPage
@@ -86,49 +88,11 @@ export async function getServerSideProps(context) {
   let file;
   let type = "";
   let tiles = [];
-  console.log("/[...path]]:getServerSideProps:context: ", context);
+  // console.log("/[...path]]:getServerSideProps:context: ", context);
   if (context.params.path && siteConfig.content[context.params.path[0]]) {
     file = context.params.path.join("/");
     let pageContent = "";
-    
-    if (!file.endsWith(".etherpad")) {
-      pageContent = await getFileContent(
-        siteConfig.content[context.params.path[0]].owner,
-        siteConfig.content[context.params.path[0]].repo,
-        siteConfig.content[context.params.path[0]].branch,
-        file
-      );
-    }
-  
-    const menuContext = () => {
-      if (siteConfig.content[context.params.path[0]].menu && siteConfig.content[context.params.path[0]].menu.collection) {
-        return siteConfig.content[siteConfig.content[context.params.path[0]].menu.collection]
-      } else {
-        return siteConfig.content[context.params.path[0]]
-      }
-    }
-
-    const pageContentText = pageContent
-      ? Buffer.from(pageContent).toString("utf-8")
-      : "";
-
-    const menuPromise = getMenuStructure(
-      siteConfig,
-      menuContext()
-    );
-    const menuStructure = await menuPromise;
-
-    console.log(
-      "/[...path]]:getServerSideProps: ",
-      collectionName(file, context.params.path[0])
-    );
-
-    const collection = collectionName(
-      file,
-      siteConfig.content[context.params.path[0]]
-    );
-    console.log("/[...path]]:getServerSideProps:collection: ", collection);
-
+    let pageContentText;
     switch (context.params.path.length) {
       case 1:
         type = "index";
@@ -148,19 +112,27 @@ export async function getServerSideProps(context) {
         });
         break;
       default:
+        if (!file.endsWith(".etherpad")) {
+          pageContent = await getFileContent(
+            siteConfig.content[context.params.path[0]].owner,
+            siteConfig.content[context.params.path[0]].repo,
+            siteConfig.content[context.params.path[0]].branch,
+            file
+          );
+        }
+    
+        pageContentText = pageContent
+          ? Buffer.from(pageContent).toString("utf-8")
+          : "";
         type = "content";
         file = context.params.path.join("/");
         break;
     }
- 
 
     return {
       props: {
         type: type,
         content: pageContentText || "",
-        // file: file,
-        // menuStructure: menuStructure || "",
-        // collection: siteConfig.content[collection.path],
         context: {
           file: file,
           ...collectionName(file, siteConfig.content[context.params.path[0]]),
@@ -169,33 +141,19 @@ export async function getServerSideProps(context) {
         key: context.params.path,
       },
     };
-  } else if (context.resolvedUrl === "/" ) {
+  } else if (context.resolvedUrl === "/") {
     type = "home";
 
     return {
       props: {
         type: type,
         content: null,
-        // file: null,
-        // menuStructure: null,
-        // collection: null,
         context: null,
         key: "home",
       },
     };
   } else {
     type = "404";
-    return { notFound: true }
-  //   return {
-  //     props: {
-  //       type: type,
-  //       content: null,
-  //       // file: null,
-  //       // menuStructure: null,
-  //       // collection: null,
-  //       context: null,
-  //       key: "404",
-  //     },
-  // }
-}
+    return { notFound: true };
+  }
 }
