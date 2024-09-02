357a724bf51be99dbbe1d20bcaa2ff210e571048
Fraser Steel <81355664+f-steel@users.noreply.github.com>
2024-09-02 18:27:16 +0000

    feat: remove previous package.json changes

--- src/_components/Editor/Editor.tsx
+++ src/_components/Editor/Editor.tsx
@@ -267,7 +267,7 @@ const Editor = React.memo(function EditorC({
 
                 // web socket provider
                 const url = new URL(window.location.href);
-                const wsUrl = `${protocol}//${url.hostname}:1234/socket.io`;
+                const wsUrl = `${protocol}//${url.hostname}:3000/socket.io`;
                 const provider = new WebsocketProvider(wsUrl, id, doc, {
                   connect: true,
                 });
